e82554e7bab8e287e3485796d1fbc3377c6e7462
Mayur Upadhayay <mayur123a@gmail.com>
2022-09-30 23:35:44 +0530

    [feat]: Initialized React project with semantic ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Contact App
+      </header>
+    </div>
+  );
+}
+
+export default App;
