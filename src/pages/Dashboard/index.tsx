b5b10daaa9b0df859733f926c99f58ca2b7a8f73
Mayur Upadhayay <mayur123a@gmail.com>
2022-10-01 23:09:39 +0530

    [feat]: Defined app skeleton

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,6 +4,10 @@ import { ReactElement } from 'react';
 // Semantic UI lib
 // import { Grid } from 'semantic-ui-react';
 
+// Atoms / Molecules / Organisms
+import Sidebar from '@/components/organisms/Sidebar';
+import MainSection from '@/components/organisms/MainSection';
+
 // CSS
 import './index.scss';
 
@@ -11,7 +15,11 @@ import './index.scss';
 export default function Dashboard(): ReactElement {
     return (
         <div className='app-container'>
-            <div className='app-dashboard'></div>
+            <div className='app-dashboard'>
+                <Sidebar
+                    contentSection={<MainSection />}
+                />
+            </div>
         </div>
     )
 };
