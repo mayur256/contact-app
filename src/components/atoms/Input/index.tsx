b409e6e37c1bd7529af2714566a0cf8e2289b570
Mayur Upadhayay <mayur123a@gmail.com>
2023-03-06 23:50:19 +0530

    chore: Stable commit of code

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,5 +1,8 @@
 // Top level imports
-import { ComponentPropsWithRef, ReactElement, SyntheticEvent } from 'react';
+import { ComponentPropsWithRef, ReactElement, SyntheticEvent, useRef } from 'react';
+
+// react-aria
+import { useTextField } from 'react-aria';
 
 // Semantic UI
 import { Input as SemInput } from 'semantic-ui-react';
@@ -16,8 +19,12 @@ export default function Input({
     onChange,
     ...rest
 }: InputType): ReactElement {
+    const ref = useRef(null);
+    const { inputProps } = useTextField({}, ref);
+
     return (
         <SemInput
+            ref={ref}
             value={value}
             onChange={onChange}
             {...rest}
