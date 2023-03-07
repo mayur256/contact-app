d1df6eabe50857b81f0e77a315108b4f07aed5e5
Mayur Upadhayay <mayur123a@gmail.com>
2023-03-07 23:31:10 +0530

    feat: Adds aria support for textfield

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,17 +1,25 @@
 // Top level imports
-import { ComponentPropsWithRef, ReactElement, SyntheticEvent, useRef } from 'react';
+import {
+    ComponentPropsWithRef, ReactElement, SyntheticEvent, useRef, forwardRef, Ref
+} from 'react';
 
 // react-aria
 import { useTextField } from 'react-aria';
+// eslint-disable-next-line no-duplicate-imports
+import type { AriaTextFieldProps } from 'react-aria';
 
 // Semantic UI
 import { Input as SemInput } from 'semantic-ui-react';
 
 // Props type definition
-export interface InputType extends ComponentPropsWithRef<typeof SemInput> {
+export interface InputType extends ComponentPropsWithRef<typeof SemInput & AriaTextFieldProps> {
     value: string;
     onChange: (event: SyntheticEvent<HTMLInputElement>) => void;
 };
+
+const SemInputWrapper = forwardRef(function SemInputWrapper(props: InputType, ref: Ref<SemInput>): ReactElement {
+    return <SemInput {...props} ref={ref} />
+});
 
 // Component definiton
 export default function Input({
@@ -21,9 +29,9 @@ export default function Input({
 }: InputType): ReactElement {
     const ref = useRef(null);
     const { inputProps } = useTextField({}, ref);
-
     return (
-        <SemInput
+        <SemInputWrapper
+            {...inputProps}
             ref={ref}
             value={value}
             onChange={onChange}
